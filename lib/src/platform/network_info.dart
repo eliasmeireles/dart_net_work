ff74b1786d9ac17b768ee22b45ae0571c1d18432
Elias Meireles <eliasmflilico@gmail.com>
2022-11-25 10:17:46 -0300

    Updated net work connect method name

--- lib/src/platform/network_info.dart
+++ lib/src/platform/network_info.dart
@@ -1,7 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 class NetworkInfo {
-  static Future<bool> isInternetConnect() async {
+  static Future<bool> hasInternetConnection() async {
     var connectivityResult = await (Connectivity().checkConnectivity());
     return connectivityResult != ConnectivityResult.none;
   }
